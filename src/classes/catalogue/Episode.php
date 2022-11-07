491d42c900fff133dc3d6e5af740d8ee3287531c
Lucie <lucye.laury@gmail.com>
2022-11-07 11:58:24 +0100

    début classes Episode

--- src/classes/catalogue/Episode.php
+++ src/classes/catalogue/Episode.php
@@ -2,7 +2,42 @@
 
 namespace iutnc\netVOD;
 
+use PDO;
+
 class Episode
 {
+
+    //ATTRIBUTS
+    private int $numero;
+    private string $titre;
+    private string $resume;
+    private int $duree;
+    private string $file;
+
+
+    //CONST
+    public function __construct(string $titre){
+
+    }
+
+
+    public static function find(string $titre, PDO $bd): Episode{
+        $c1 = $bd->prepare("Select * from episode where titre= :ti ;");
+        $c1->bindParam(":ti",$titre);
+        $c1->execute();
+        $p=null;
+        $creer=false;
+        while ($d = $c1->fetch())
+        {
+            if (!$creer) {
+                $p = new Playlist($d['nom']);
+                $creer = true;
+            }
+            $al = new AlbumTrack($d['titre'],$d['nom']);
+            $al->duree=$d['duree'];
+            $p->addPiste($al);
+        }
+        return $p;
+    }
 
 }
