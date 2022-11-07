63259e243e89ae21992b70371c3a36cf81d1ae68
Egario-akier <thierry.dissidia@gmail.com>
2022-11-07 17:44:53 +0100

    Ajout de la fonction updateListeEnCours dans User

--- src/classes/user/User.php
+++ src/classes/user/User.php
@@ -1,6 +1,8 @@
 <?php
 namespace iutnc\netVOD\user;
 use iutnc\netVOD\catalogue\Serie;
+use iutnc\netVOD\catalogue\Episode as Episode;
+use iutnc\netVOD\db\ConnectionFactory;
 use iutnc\netVOD\exception\ProprieteInexistanteException;
 
 class User
@@ -28,6 +30,35 @@ class User
     public function __get(string $attribut):mixed {
         if (property_exists ($this, $attribut)) return $this->$attribut;
         throw new ProprieteInexistanteException ("$attribut: propriété inexistante");
+    }
+
+    /**
+     * @param Episode $e épisode ajouté à la série
+     * @return void
+     */
+    public function updateListeEnCours(Episode $e): void
+    {
+        $db = ConnectionFactory::makeConnection();
+        $titreEpisode = $e->titre;
+        $req = $db->prepare("SELECT serie_id,titre from serie 
+             INNER JOIN episode ON episode.serie_id = serie.id
+             where episode.titre=?;");
+        $req->bindParam("?", $titreEpisode);
+        $req->execute();
+        $row = $req->fetch();
+        $idserie = $row['serie_id'];
+        $titreSerie = $row['titre'];
+        $trouveSerie = false;
+        //pour chaque serie dans la liste EnCours
+        foreach ($this->enCours as $serieEnCours){
+            if($serieEnCours->id==$idserie){
+                $trouveSerie = true;
+                break;
+            }
+        }
+        if(!$trouveSerie){
+            $enCours[]= Serie::find($titreSerie);
+        }
     }
 
 
