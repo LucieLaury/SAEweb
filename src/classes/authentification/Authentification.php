1797c9a93b6c185adf9a86706212a3fdc45627e0
drakoghir <gdardenne0@gmail.com>
2022-11-07 17:42:52 +0100

    Ajout des méthodes register et connexion

--- src/classes/authentification/Authentification.php
+++ src/classes/authentification/Authentification.php
@@ -1,8 +1,34 @@
 <?php
 
+use iutnc\netVOD\db\ConnectionFactory;
+use iutnc\netVOD\exception\AlreadyRegisteredEmailException;
+use iutnc\netVOD\exception\BadPasswordException;
+
 class Authentification
 {
-    public static function register(string $email, string $password) : void {}
+    /**
+     * @throws BadPasswordException
+     * @throws AlreadyRegisteredEmailException
+     */
+    public static function register(string $email, string $password) : void {
+        $bd = ConnectionFactory::makeConnection();
+        // Verify strength of password
+        if(!self::checkPassStrength($password, 10)) throw new BadPasswordException("Le mot de passe ne correspond pas à nos critères");
+        // Verify if email already exist
+        $query = $bd->prepare("SELECT * FROM User WHERE mail = ?");
+        $query->execute($email);
+        if($query->rowCount() > 0) throw new AlreadyRegisteredEmailException("Cet email est déjà enregistré");
+        $query->closeCursor();
+        // Sanitize the email
+        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        // Hash the password
+        $passwordHash =password_hash($password, PASSWORD_DEFAULT, ['cost' => 12]);
+        // Insert the user in database
+        $insert = $bd->prepare("INSERT INTO User (mail, password) value (?, ?)");
+        $insert->execute($email, $passwordHash);
+        $query->closeCursor();
+        $insert->closeCursor();
+    }
 
     public static function checkPassStrength(string $password, int $minLength) : bool {
         $length = strlen($password) > $minLength;
@@ -22,7 +48,17 @@ class Authentification
 
     public static function activate(string $token) : bool {return false;}
 
-    public static function authenticate(string $email, string $passwd2check): void {}
+    /**
+     * @throws BadPasswordException
+     */
+    public static function authenticate(string $email, string $passwd2check): void {
+        $bd = \iutnc\deefy\db\ConnectionFactory::makeConnection();
+        $query = $bd->prepare("select * from User where email = ? ");
+        $query->execute($email);
+        $data = $query->fetch(PDO::FETCH_ASSOC);
+        $hash = $data['passwd'];
+        if(!password_verify($passwd2check, $hash)) throw new BadPasswordException("Le mot de passe ou l'identifiant saisi est incorrecte");
+    }
 
 
 }
---
+++ src/classes/exception/BadPasswordException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace iutnc\netVOD\exception;
+
+use Exception;
+
+class BadPasswordException extends Exception
+{
+
+}
