34ea373d6b47ee652aab39347f0a8fa27633a2a1
Nathan <Nathan.Melbeck@gmail.com>
2022-11-07 16:56:56 +0100

    accUser

--- src/classes/afficheur/AccueilUser.php
+++ src/classes/afficheur/AccueilUser.php
@@ -7,15 +7,21 @@ class accueilUser extends Afficheur
 
     public function execute(): string
     {
-        if ($this->http_method=="GET"){
-
+        if ($this->http_method == "GET") {
+            $res = "";
+            $user = $_SESSION['user'];
+            $user = unserialize($user);
+            $list = $user->favoris;
+            foreach ($list as $value) {
+                $res .= $value->render();
+            }
+            return $res;
         }
-        else if( $this->http_method=="POST"){
-
+        else
+            if ($this->http_method == "POST") {
+                return "";
+            }
+            return "";
         }
-        return "";
-    }
-
-
 
 }
