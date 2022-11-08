e9c1f50c46b658688b13cb779ff5ad5982f6139a
Lucie <lucye.laury@gmail.com>
2022-11-08 15:50:06 +0100

    partie info sur la serie de RenderEpisode.php

--- src/classes/render/RenderEpisode.php
+++ src/classes/render/RenderEpisode.php
@@ -17,11 +17,20 @@ class RenderEpisode
     public function render():string{
         $img = $this->s->__get("img");
         $titre = $this->s->titre;
+        $desc = $this->s->descriptif;
+        $annee = $this->s->annee;
+        $date = $this->s->date;
+
         $res="<div style='display: flex; flex-direction: row;'>";
 
             $res.="<img src='$img' width='25%'/>";
-            $res.="<div style='text-align: center'>
+            $res.="<div style='text-align: center; margin-left: 30px;'>
                 <p><strong>$titre</strong></p>
+                <p>genre : </p>
+                <p>public visé : </p>
+                <p>descriptif : $desc</p>
+                <p>année de sortie : $annee</p>
+                <p>date d'ajout : $date</p>
             </div>";
 
 
