7c1923e58f0bd12fc8a55cc57b73ae4fa64b008e
miesch3u <nathanaelmiesch@gmail.com>
2022-11-08 15:15:38 +0100

    debug des commentaires

--- src/classes/afficheur/AfficheurCommentaires.php
+++ src/classes/afficheur/AfficheurCommentaires.php
@@ -5,12 +5,16 @@ namespace iutnc\netVOD\afficheur;
 use iutnc\netVOD\db\ConnectionFactory;
 use PDO;
 
-class AfficheurCommentaires extends Afficheur {
+class AfficheurCommentaires extends Afficheur
+{
 
     private static $bd;
+    private bool $edit;
 
-    public function __construct(){
+    public function __construct()
+    {
         parent::__construct();
+        $this->edit = false;
         AfficheurCommentaires::$bd = ConnectionFactory::makeConnection();
     }
 
@@ -21,48 +25,66 @@ class AfficheurCommentaires extends Afficheur {
         $user = $_SESSION['user'];
         $user = unserialize($user);
         $mail = $user->__get('email');
-        if($_SERVER['REQUEST_METHOD'] === "POST"){
-            //comme la methode post est utilisée pour supprimer, ajouter ou modifier un commentaire un commence par supprimer l'ancien
-            //s'il n'existe pas, on supprime 0 lignes et donc ne pose pas de probleme
-            //si on souhaite le modifier, cela permet de partire d'une base saine
-            $query = AfficheurCommentaires::$bd->prepare("delete from Feedback where mail = ? and idSerie = ?");
-            $query->execute($mail,$_GET['idSerie']);
-            if (!isset($_POST['rmv'])){
-                //on entre ici uniquement si on souhaite pas supprimer le commentaire
+        if ($_SERVER['REQUEST_METHOD'] === "POST") {
+            if (isset($_POST['valEdit'])) {
+                $query = AfficheurCommentaires::$bd->prepare("update Feedback set txtComm = ? where mail = ? and idSerie = ?");
+                $comm = filter_var($_POST['comm'], FILTER_SANITIZE_STRING);
+                $comm = nl2br($comm);
+                $query->bindparam(1, $comm);
+                $query->bindparam(2, $mail);
+                $query->bindparam(3, $_GET['idSerie']);
+                $query->execute();
+                $this->edit = false;
+            } else if (isset($_POST['rmv'])) {
+                $query = AfficheurCommentaires::$bd->prepare("delete from Feedback where mail = ? and idSerie = ?");
+                $query->bindparam(1, $mail);
+                $query->bindparam(2, $_GET['idSerie']);
+                $query->execute();
+            } elseif (isset($_POST['add'])) {
                 $query = AfficheurCommentaires::$bd->prepare("insert into Feedback (mail,txtComm,idSerie) values (?,?,?)");
-                $comm = filter_var($_POST['comm'],FILTER_SANITIZE_STRING);
-                $query->execute($mail,$comm,$_GET['idSerie']);
-            }
+                $comm = filter_var($_POST['comm'], FILTER_SANITIZE_STRING);
+                $comm = nl2br($comm);
+                $query->bindparam(1, $mail);
+                $query->bindparam(2, $comm);
+                $query->bindparam(3, $_GET['idSerie']);
+                $query->execute();
+            } else $this->edit = true;
         }
         $query = AfficheurCommentaires::$bd->prepare("select mail,txtComm from Feedback where idSerie = ?");
-        $query->bindParam(1,$_GET['idSerie']);
+        $query->bindParam(1, $_GET['idSerie']);
         $query->execute();
         $alreadyCommented = false;
         $res = "Commentaires de votre serie :</br>";
-        while ($data = $this->query->fetch(PDO::FETCH_ASSOC)){
+        while ($data = $query->fetch(PDO::FETCH_ASSOC)) {
             //parcours de tous les commentaires de la serie
-            $res.= $data["mail"]."</br>";
+            $res .= $data["mail"] . "</br>";
             if ($mail == $data["mail"]) {
                 // on verifie si le commentaire courant a ete poste par l'utilisateur courant
                 // cela permet de lui donner l'option de modifier ou supprimer son commentaire ainsi que de l'empecher d'en poser un nouveau
                 $alreadyCommented = true;
-                $t = $data['txtComm']."</br>";
-                $res .= "<form method=post>"
-                    . "<button name='edit' type=submit>modifier commentaire</button><button name='rmv' type='submit'>supprimer commentaire</button><br>"
-                    . "<input name='comm' type='text' content=$t>"
-                . "</form>";
-            }
-            else $res.="</br>".$data['txtComm']."</br>";
+                if ($this->edit) {
+                    $res .= "<form method=post>"
+                        . "<button name='valEdit' type=submit>valider</button><br>"
+                        . "<textarea type=text name=comm placeholder='votre commentaire' style='min-height: 100px;min-width: 400px;max-height: 100px;max-width: 400px;'></textarea>"
+                        . "</form>";
+                } else {
+                    $res .= "<form method=post>"
+                        . "<button name='edit' type=submit>modifier commentaire</button><button name='rmv' type='submit'>supprimer commentaire</button><br>"
+                        . "</form>";
+                }
+            } else $res .= "</br>";
+            $res .= $data['txtComm'] . "</br>";
+            $res .= "</br></br>";
         }
+        $id = $_GET['idSerie'];
         if (!$alreadyCommented) {
             //si l'utilisateur n'as pas encore commenter cette serie, on place un champ prevu a cet effet
-            $id = $_GET['idSerie'];
             $res .= "<form method=post>"
-                . "<input type=text name=comm placeholder='votre commentaire' required>"
-                . "<button type=submit>commenter</button>"
+                . "<textarea type=text name=comm placeholder='votre commentaire' style='min-height: 100px;min-width: 400px;max-height: 100px;max-width: 400px;'></textarea>"
+                . "</br><button type=submit name='add'>commenter></button>"
                 . "</form>";
         }
-        $res.= "<button href=?action=afficher-episode?idSerie=$id text='retour à la serie'>";
+        $res .= "<button href=?action=afficher-episode?idSerie=$id >retour à la serie</button>";
         return $res;
     }
 }
