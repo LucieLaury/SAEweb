0f41192bf85b4ea68dfa31e659b272b1b855dfce
Lucie <lucye.laury@gmail.com>
2022-11-09 14:54:13 +0100

    AfficheurCatalogue.php avec plus de trierFilms, incomplet

--- src/classes/afficheur/AfficheurCatalogue.php
+++ src/classes/afficheur/AfficheurCatalogue.php
@@ -39,7 +39,6 @@ class AfficheurCatalogue extends Afficheur
             $titre = $serieC->__get("titre");
             $re = new RenderSerie($serieC);
             $html .= $re->render();
-
         }
 
         $html .="</div>";
@@ -52,9 +51,9 @@ class AfficheurCatalogue extends Afficheur
         $html .= "<form method='post' action='?action=afficher-catalogue'>";
         $html .= "<input type='search' name='rech' placeholder='rechercher un film' style='width: 50%; margin-right: 10px'/>";
         $html .= "<input type='submit' name='submit' value='Envoyer' /><br>";
-        $html .= "<label>Trie par : </label> <input type='radio' name='all' value='titre'/><label style='margin-right:40px'>titre</label>";
-        $html .= "<input type='radio' name='all' value='date'/> <label style='margin-right:40px'>Date</label>";
-        $html .= "<input type='radio' name='all' value='nbEpisode'/><label style='margin-right:40px'>nombre d'Episode</label>";
+        $html .= "<label>Trie par : </label> <input type='radio' name='trie' value='titre'/><label style='margin-right:40px'>titre</label>";
+        $html .= "<input type='radio' name='trie' value='date'/> <label style='margin-right:40px'>Date</label>";
+        $html .= "<input type='radio' name='trie' value='nbEpisodes'/><label style='margin-right:40px'>nombre d'Episode</label>";
         $html .= "</form>";
         $html .= "</div>";
 
@@ -65,7 +64,8 @@ class AfficheurCatalogue extends Afficheur
 
     public function afficherRecherche():string{
 
-        $res = "<div style='display: flex; flex-direction: row; justify-content: space-around'>";
+        $res = $this->affichageFormulaire();
+        $res .= "<div style='display: flex;justify-content: space-around; flex-direction: row; flex-wrap: wrap'>";
         //séparation des mots de la methode post
         $tab = explode(' ', $_POST['rech']);
         //tableau qui répertorie les titres des series qui vont etre affichees
@@ -74,9 +74,8 @@ class AfficheurCatalogue extends Afficheur
         for($i=0; $i<count($tab); $i++){
             $mot = $tab[$i];
             $tab1 = $this->requeteRech($mot, "titre", $seriesAffichees);
-            $res .= $tab1[1];
-            $tab2 = $this->requeteRech($mot, "descriptif", $tab1[0]);
-            $res .= $tab2[1];
+            $tab2 = $this->requeteRech($mot, "descriptif", $tab1);
+            $res .= $this->trierFilms($_POST['trie'], $tab2);
         }
         $res .="</div>";
         return $res;
@@ -87,20 +86,50 @@ class AfficheurCatalogue extends Afficheur
         $req = $db->query("SELECT titre from serie where $retour like '%".$mot."%' ");
         $req->execute();
 
-        $res ="";
         while ($row = $req->fetch()){
-
             if (!isset($serieAff[$row['titre']])){
                 $serie = Serie::find($row['titre']);
-                $re = new RenderSerie($serie);
-                $res .= $re->render();
                 $serieAff[$row['titre']]=$serie;
             }
         }
-        $tab = [];
-        $tab[0] = $serieAff;
-        $tab[1] = $res;
-        return $tab;
+        return $serieAff;
     }
+
+    /**
+     * methode qui trie le tableau des séries à afficher dans l'ordre du trie voulu. Soit sur le titre
+     * soit sur la date ou le nombre d'épisode de la série
+     * @param string $trie qui prend soit les valeurs "titre", "date", "nbEpisodes"
+     * @param array $serieAff le tableau des séries qui vont être affichées
+     * @return string l'html d'affichage des séries.
+     */
+    public function trierFilms(string $trie="titre", array $serieAff):string{
+        $tabtrie=[];
+
+        for ($i=1; $i<count($serieAff)-1;$i++){
+            $val1 = $serieAff[$i]->__get($trie);
+            $min = $val1;
+            for ($j=$i+1;$j<count($serieAff);$j++){
+                $valeur = $serieAff[$j]->__get($trie);
+                if ($valeur < $min){
+                    $min = $valeur;
+                }
+            }
+            if ($min > $val1)
+        }
+
+        $res="";
+
+        ksort($serieAff);
+
+        foreach ($serieAff as $serie) {
+            $re = new RenderSerie($serie);
+            $res .= $re->render();
+        }
+        return $res;
+    }
+
+
+
+
 
 }
