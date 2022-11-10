7e5c144f8a20f938d454f9de188682d5581a5e31
miesch3u <nathanaelmiesch@gmail.com>
2022-11-10 11:23:03 +0100

    protection conte les notes négatives

--- src/classes/afficheur/AfficheurSerie.php
+++ src/classes/afficheur/AfficheurSerie.php
@@ -111,6 +111,7 @@ class AfficheurSerie extends Afficheur
     }
 
     ////partie Nathanael:
+
     private function noteEtComms(): string{
         $res = "<div style='display: flex; flex-direction: column; margin-left: 50px;margin-right: 50px'>";
         $user = $_SESSION['user'];
@@ -165,7 +166,7 @@ class AfficheurSerie extends Afficheur
         }
         if(!$alreadyNoted || $this->updatingNote){
             $res.= "<form method='post'>".
-                    "<input type='number' class='shadow rounded' name='note' placeholder='note /5' max='5'>".
+                    "<input type='number' class='shadow rounded' name='note' placeholder='note /5' max='5' min='0'>".
                     "<button class='mx-auto block shadow rounded-2xl p-1 px-3 font-medium block mx-2 mt-5 bg-gradient-to-r from-green-400 to-blue-500 text-white hover:from-blue-500 hover:to-green-400' name='Bnote' type='submit'>Noter</button>";
         }
         else{
